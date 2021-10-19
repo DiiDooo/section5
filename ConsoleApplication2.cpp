45c6bbc29219e1806f4895de4c3dd12df306fb14
diidooo <ma7moud.3laa.7@Gmail.com>
2021-10-19 17:27:00 +0200

    First project

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,15 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b
+		cout << "enter two numbers:" << endl;
+	    cin >> a >> b;
+	    cout << a << " + " << b << " = " << a + b << endl;
+    return 0;
+}
+
